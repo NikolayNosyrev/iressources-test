dabae0341242e6144dffb1de9ce73fc262859341
NikolayNosyrev <seversherif2@mail.ru>
2017-12-11 21:09:00 +0300

    Оптимизация

--- src/AppBundle/Repository/RecipeRepository.php
+++ src/AppBundle/Repository/RecipeRepository.php
@@ -14,6 +14,7 @@ class RecipeRepository extends EntityRepository
     public function findByString($search)
     {
         return $this->createQueryBuilder('r')
+            ->join('r.ingredients', 'i')
             ->where('r.name LIKE :search')
             ->setParameter('search', '%'.$search.'%')
             ->getQuery()
